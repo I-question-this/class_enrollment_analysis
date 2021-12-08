5c856b57b2f9aea1b1b42e598263996c4a8fb2c7
Tyler Westland <tylerofthewest@gmail.com>
2021-12-08 11:12:15 -0500

    First commit of some hard hitting journalism.

---
+++ README.md
@@ -0,0 +1,24 @@
+# Enrollment Ratios
+I was curious if there was low enrollment this semester.
+Thus I collected all the enrollment information for classes in the CS 
+department on 2021-12-08.
+Note that some of the courses have more than maximum enrollment, that is
+not a clerical error.
+Also, note that I did not include self study, research, thesis courses as
+those are dependent on an individual student's career and are not canceled
+from low enrollment.
+I did include Senior Design and graduate seminars though, as those are 
+required for enrollment.
+
+
+Over all, it seems like enrollment is as to be expected.
+There is high undergrad enrollment, with the median at ~83%.
+The grad enrollment is lower, with the median at ~63%, but every semester
+there are complaints that graduate courses don't get enough enrollment to
+not be cancelled.
+The overall enrolment is at about ~83%, which is because most of the
+graduate courses are actually combined courses with undergrads.
+There are little graduate exclusive courses compared to undergraduate 
+  exclusive courses.
+
+Note, I did not take the time to collect data from the fall semester.
---
+++ main.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+"""Make some plots of the class enrollment data"""
+
+__author__="Tyler Westland"
+
+import argparse
+from dataclasses import dataclass, field
+import json
+import matplotlib.pyplot as plt
+import os
+import sys
+
+
+@dataclass(frozen=True)
+class Course:
+    department: str
+    number: int
+    extra_number_info: str
+    name: str
+    section: int
+    capacity: int
+    enrolled: int
+    wait_list_capacity: int
+    wait_list_total: int
+
+    @classmethod
+    def from_dict(cls, d:dict) -> "Course":
+        return cls(
+                d["department"],
+                int(d["number"]),
+                d["extra_number_info"],
+                d["name"],
+                int(d["section"]),
+                int(d["capacity"]),
+                int(d["enrolled"]),
+                int(d["wait list capacity"]),
+                int(d["wait list total"])
+                )
+
+    @property
+    def enrollement_ratio(self) -> float:
+        return self.enrolled / self.capacity
+
+def box_plot(output_name, x_name, x_data, show):
+    fig, ax = plt.subplots()
+    ax.boxplot(x_data, vert=False)
+
+    ax.set_title(f"{x_name}")
+
+    if show:
+        plt.show()
+    fig.savefig(output_name)
+    plt.close()
+
+
+def parse_arguments(args=None) -> argparse.Namespace:
+    """Returns the parsed arguments.
+    Parameters
+    ----------
+    args: List of strings to be parsed by argparse.
+        The default None results in argparse using the values passed into
+        sys.args.
+    """
+    parser = argparse.ArgumentParser(
+            description="Make some plots of the class enrollment data",
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-df","--data_file", help="Path to the data file.",
+                        default="./class_enrollment.json")
+    parser.add_argument("-s", "--show", help="Interactively show the plots",
+                        default=False, action="store_true")
+    args = parser.parse_args(args=args)
+    return args
+
+
+def main(data_file:str="./class_enrollment.json", show: bool=False) -> None:
+    """Main function.
+
+    Parameters
+    ----------
+    data_file: str
+        Path to the data file.
+    show: bool=False
+        Rather to show the plots interactively.
+    Returns
+    -------
+    ???
+        Something useful.
+    Raises
+    ------
+    FileNotFoundError
+        Means that the input file was not found.
+    """
+    # Error check if the file even exists
+    if not os.path.isfile(data_file):
+        raise FileNotFoundError("File not found: {}".format(input_file))
+
+    # Read in the data
+    with open(data_file) as fin:
+        enrollment = json.load(fin)
+
+    # Change each dict into a proper class
+    for i in range(len(enrollment["class information"])):
+        enrollment["class information"][i] = Course.\
+                from_dict(enrollment["class information"][i])
+
+    box_plot("undergrad.png", "Undergrad Enrollment", 
+             [course.enrollement_ratio for course in 
+               filter(lambda course: course.number < 6000, 
+                      enrollment["class information"])
+             ],
+             show)
+    box_plot("grad.png", "Grad Enrollment", 
+             [course.enrollement_ratio for course in 
+               filter(lambda course: course.number >= 6000, 
+                      enrollment["class information"])
+             ],
+             show)
+    box_plot("all.png", "All Enrollment", 
+             [course.enrollement_ratio for course in 
+                enrollment["class information"]
+             ],
+             show)
+
+    return None
+
+
+def cli_interface() -> None:
+    """Get program arguments from command line and run main"""
+    args = parse_arguments()
+    try:
+        main(**vars(args))
+        sys.exit(0)
+    except FileNotFoundError as exp:
+        print(exp, file=sys.stderr)
+        sys.exit(1)
+
+
+# Execute only if this file is being run as the entry file.
+if __name__ == "__main__":
+    cli_interface()
